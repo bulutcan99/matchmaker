1c82d26df3cf6bf68712e824151c45de6b8af4b0
bulutcan99 <gocerbulutcan@gmail.com>
2024-09-17 12:27:44 +0300

    refactor

--- src/adapter/driven/storage/db/repository/user.rs
+++ src/adapter/driven/storage/db/repository/user.rs
@@ -19,30 +19,32 @@ impl UserRepository {
         UserRepository { db }
     }
 }
-
 #[async_trait]
 impl UserRepo for UserRepository {
     async fn save(&self, user: &User) -> Result<Uuid, Error> {
         let saved_user_id = sqlx::query_scalar!(
-    r#"
-        INSERT INTO "user" (id, name, surname, email, role, password_hash, is_blocked, is_verified, google_oauth_id, created_at, updated_at)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-        RETURNING id
-    "#,
-    user.id,
-    user.name,
-    user.surname,
-    user.email,
-    user.role.as_string(),
-    user.password_hash.as_string(),
-    user.is_blocked,
-    user.is_verified,
-    user.google_oauth_id,
-    Timestamp::now_utc().convert_to_offset(),
-    Timestamp::now_utc().convert_to_offset(),
-)
-.fetch_one(&*self.db)
-.await?;
+            r#"
+            INSERT INTO "user" (id, name, surname, email, role, password_hash, reset_token, reset_sent_at, email_verification_token, email_verification_sent_at, email_verified_at, blocked_at, created_at, updated_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+            RETURNING id
+            "#,
+            user.id,
+            user.name,
+            user.surname,
+            user.email,
+            user.role.as_string(),
+            user.password_hash.as_string(),
+            user.reset_token,
+            user.reset_sent_at,
+            user.email_verification_token,
+            user.email_verification_sent_at,
+            user.email_verified_at,
+            user.blocked_at,
+             Timestamp::now_utc().convert_to_offset(), // Assume this method is correct for your context
+             Timestamp::now_utc().convert_to_offset()  // Assume this method is correct for your context
+        )
+        .fetch_one(&*self.db)
+        .await?;
 
         Ok(saved_user_id)
     }
@@ -50,8 +52,8 @@ impl UserRepo for UserRepository {
     async fn update(&self, id_str: &str, user: &User) -> Result<User, Error> {
         let id = Uuid::parse_str(id_str)?;
         let updated_user = sqlx::query_as!(
-        User,
-        r#"
+            User,
+            r#"
             UPDATE "user"
             SET
                 name = COALESCE($2, name),
@@ -59,26 +61,32 @@ impl UserRepo for UserRepository {
                 email = COALESCE($4, email),
                 role = COALESCE($5, role),
                 password_hash = COALESCE($6, password_hash),
-                updated_at = COALESCE($7, updated_at),
-                is_blocked = COALESCE($8, is_blocked),
-                is_verified = COALESCE($9, is_verified),
-                google_oauth_id = COALESCE($10, google_oauth_id)
+                reset_token = COALESCE($7, reset_token),
+                reset_sent_at = COALESCE($8, reset_sent_at),
+                email_verification_token = COALESCE($9, email_verification_token),
+                email_verification_sent_at = COALESCE($10, email_verification_sent_at),
+                email_verified_at = COALESCE($11, email_verified_at),
+                blocked_at = COALESCE($12, blocked_at),
+                updated_at = COALESCE($13, updated_at)
             WHERE id = $1
-            RETURNING id, name, surname, email, role as "role: _", password_hash, created_at, updated_at, is_blocked, is_verified, google_oauth_id
-        "#,
-        id,
-        user.name,
-        user.surname,
-        user.email,
-        user.role.as_string(),
-        user.password_hash.as_string(),
-        Timestamp::now_utc().convert_to_offset(),
-        user.is_blocked,
-        user.is_verified,
-        user.google_oauth_id
-)
-.fetch_one(&*self.db)
-.await?;
+            RETURNING id, name, surname, email, role as "role: _", password_hash, reset_token, reset_sent_at, email_verification_token, email_verification_sent_at, email_verified_at, blocked_at, created_at, updated_at
+            "#,
+            id,
+            user.name,
+            user.surname,
+            user.email,
+            user.role.as_string(),
+            user.password_hash.as_string(),
+            user.reset_token,
+            user.reset_sent_at,
+            user.email_verification_token,
+            user.email_verification_sent_at,
+            user.email_verified_at,
+            user.blocked_at,
+             Timestamp::now_utc().convert_to_offset() // Assume this method is correct for your context
+        )
+        .fetch_one(&*self.db)
+        .await?;
 
         Ok(updated_user)
     }
@@ -90,7 +98,7 @@ impl UserRepo for UserRepository {
             r#"
             DELETE FROM "user"
             WHERE id = $1
-        "#,
+            "#,
             id
         )
         .execute(&*self.db)
@@ -102,14 +110,14 @@ impl UserRepo for UserRepository {
     async fn find_all(&self) -> Result<Vec<User>, Error> {
         sqlx::query_as!(
             User,
-           r#"
-        SELECT id, name, surname, email, role, password_hash, created_at, updated_at, is_blocked, is_verified, google_oauth_id
-        FROM "user"
-    "#
+            r#"
+            SELECT id, name, surname, email, role, password_hash, reset_token, reset_sent_at, email_verification_token, email_verification_sent_at, email_verified_at, blocked_at, created_at, updated_at
+            FROM "user"
+            "#
         )
         .fetch_all(&*self.db)
         .await
-        .map_err(|err| anyhow!("Error from getting user by id: {}", err))
+        .map_err(|err| anyhow!("Error getting users: {}", err))
     }
 
     async fn find_by_id(&self, id_str: &str) -> Result<Option<User>, Error> {
@@ -117,29 +125,27 @@ impl UserRepo for UserRepository {
         let row = sqlx::query_as!(
             User,
             r#"
-        SELECT id, name, surname, email, role, password_hash, created_at, updated_at, is_blocked, is_verified, google_oauth_id
-        FROM "user" WHERE id = $1
-        "#,
+            SELECT id, name, surname, email, role, password_hash, reset_token, reset_sent_at, email_verification_token, email_verification_sent_at, email_verified_at, blocked_at, created_at, updated_at
+            FROM "user"
+            WHERE id = $1
+            "#,
             id
         )
         .fetch_optional(&*self.db)
         .await
-        .map_err(|err| anyhow!("Error from getting user by id: {}", err))?;
+        .map_err(|err| anyhow!("Error getting user by id: {}", err))?;
 
-        if let Some(user) = row {
-            Ok(Some(user))
-        } else {
-            Ok(None)
-        }
+        Ok(row)
     }
 
     async fn find_by_email(&self, email: &str) -> Result<Option<User>, Error> {
         let user = sqlx::query_as!(
             User,
             r#"
-        SELECT id, name, surname, email, role, password_hash, created_at, updated_at, is_blocked, is_verified, google_oauth_id
-        FROM "user" WHERE email = $1
-        "#,
+            SELECT id, name, surname, email, role, password_hash, reset_token, reset_sent_at, email_verification_token, email_verification_sent_at, email_verified_at, blocked_at, created_at, updated_at
+            FROM "user"
+            WHERE email = $1
+            "#,
             email
         )
         .fetch_optional(&*self.db)
