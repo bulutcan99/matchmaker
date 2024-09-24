6281d35113d2782b6050197e810b9294f6e82248
bulutcan99 <gocerbulutcan@gmail.com>
2024-09-24 13:14:51 +0300

    refactor

--- src/adapter/driving/presentation/http/router.rs
+++ src/adapter/driving/presentation/http/router.rs
@@ -6,6 +6,7 @@ use axum::Router;
 use tower_cookies::CookieManagerLayer;
 
 use crate::adapter::driving::presentation::http::handler::_default::health_check_handler::health_checker_handler;
+use crate::adapter::driving::presentation::http::handler::auth;
 use crate::adapter::driving::presentation::http::handler::auth::login::login_handler;
 use crate::adapter::driving::presentation::http::handler::auth::me::me_handler;
 use crate::adapter::driving::presentation::http::handler::auth::register::register_handler;
@@ -37,14 +38,22 @@ pub fn make_router<S>(app_state: Arc<AppState<S>>) -> Router
 where
     S: UserManagement + 'static,
 {
-    let protected_routes = Router::new().route(
-        "/api/v1/users/me",
-        get(me_handler).layer(from_fn_with_state(app_state.clone(), is_authenticated)),
-    );
+    let protected_routes = Router::new()
+        .route(
+            "/api/v1/users/me",
+            get(me_handler).layer(from_fn_with_state(app_state.clone(), is_authenticated)),
+        )
+        .route(
+            "/api/v1/companies/register",
+            post(register_handler).layer(from_fn_with_state(app_state.clone(), is_authenticated)),
+        );
 
     let public_routes = Router::new()
         .route("/api/v1/healthchecker", get(health_checker_handler))
-        .route("/api/v1/auth/register", post(register_handler))
+        .route(
+            "/api/v1/auth/register",
+            post(auth::register::register_handler),
+        )
         .route("/api/v1/auth/login", post(login_handler));
 
     Router::new()
